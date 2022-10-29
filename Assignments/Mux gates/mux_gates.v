eca7f79425e5feaf980ada6910cf609e851d1a09
Adhi-cyber <112969103+Adhi-cyber@users.noreply.github.com>
2022-10-29 21:54:47 +0530

    Add files via upload

---
+++ Assignments/Mux gates/mux_gates.v
@@ -0,0 +1,23 @@
+module mux_2x1_gates(a,b,output_and,output_or,output_not);
+   
+input a,b;
+output reg output_and,output_or,output_not;
+  
+always @(a,b)
+     begin
+     
+       if(a==1'b0)
+         begin
+           output_and = 1'b0;
+           output_or = b;
+           output_not = 1'b1;
+         end
+     
+       else
+         begin
+            output_and = b;
+            output_or = 1'b1;
+            output_not = 1'b0;
+         end
+  end
+endmodule
